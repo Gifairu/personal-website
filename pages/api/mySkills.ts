18aab9f77197fa1a4b55c3a813cf33636e92725e
Gifairu <gifairu@gmail.com>
2021-06-09 06:45:54 +0800

    Adding API for Home

---
+++ pages/api/mySkills.ts
@@ -0,0 +1,33 @@
+const mySkills = [
+	{
+		name: 'JavaScript',
+		image: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png'
+	},
+	{
+		name: 'TypeScript',
+		image:
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png'
+	},
+	{
+		name: 'NodeJS',
+		image:
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1280px-Node.js_logo.svg.png'
+	},
+	{
+		name: 'ReactJS',
+		image:
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'
+	},
+	{
+		name: 'NextJS',
+		image:
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Nextjs-logo.svg/1200px-Nextjs-logo.svg.png'
+	},
+	{
+		name: 'Svelte',
+		image:
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/1200px-Svelte_Logo.svg.png'
+	}
+];
+
+export default mySkills;
---
+++ pages/api/socialMedia.ts
@@ -0,0 +1,21 @@
+const socialMedia = [
+	{
+		name: 'Facebook',
+		image:
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1365px-Facebook_f_logo_%282019%29.svg.png',
+		link: 'https://facebook.com/Gifairu'
+	},
+	{
+		name: 'E-mail',
+		image:
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/1024px-Gmail_icon_%282020%29.svg.png',
+		link: 'mailto:gifairu@gmail.com'
+	},
+	{
+		name: 'Github',
+		image: 'https://image.flaticon.com/icons/png/512/25/25231.png',
+		link: 'https://github.com/Gifairu'
+	}
+];
+
+export default socialMedia;
